e4547fb3630d96db94503769bf86bb0af41405b5
Pablo Zagnoli <pablozagnoli@outlook.com>
2022-10-27 20:00:49 -0300

    add routamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile/profile.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
